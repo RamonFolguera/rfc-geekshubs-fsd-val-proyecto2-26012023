b1149079672164e3be28c8eac41416e3b940e105
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-01-29 12:20:02 +0100

    Estructura del proyecto completa. Pasando a mejorar cada uno de los elementos estructurados

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 
 
-
-// const addRemoveClass = () => {
-//     let nav2Div = document.querySelector(".nav-2");
-//     if (nav2Div.className === "col-2 col-sm-8 col-lg-6 class nav-2 navbar-nav text-lg-start text-sm-start text-center pt-5") {
-//       nav2Div.className += " responsive";
-//     } else {
-//       nav2Div.className = "col-2 col-sm-8 col-lg-6 class nav-2 navbar-nav text-lg-start text-sm-start text-center pt-5";
-//     }
-// console.log(nav2Div)
-// }
-
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "col-6 col-sm-8 col-lg-6 nav-2 navbar-nav text-lg-start text-sm-start text-center pt-5 topnav") {
